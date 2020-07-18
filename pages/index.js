ef9d7d9032c966ae7e2ece7214ed80f99e265aec
Calvin Cheung <calvchum@gmail.com>
2020-07-19 02:46:47 +1000

    installed styled-components and babel, updated babel config to enable SSR styled components

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,12 @@
+import styled, { createGlobalStyle } from "styled-components";
+
+const Container = styled.div`
+	text-align: center;
+`;
+
 const Index = () => (
-	<div>
-		<p>Hello World!</p>
-	</div>
+	<Container>
+		<p>Hello, World!</p>
+	</Container>
 );
 export default Index;
